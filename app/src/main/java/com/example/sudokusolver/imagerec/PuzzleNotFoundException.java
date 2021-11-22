14574e093e2c23faf0b33e7120ddbb9cb92397fa
hkaug <hkaug2001@gmail.com>
2021-11-22 06:15:15 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/sudokusolver/imagerec/PuzzleNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.sudokusolver.imagerec;
+
+public class PuzzleNotFoundException extends Exception {
+    public PuzzleNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ app/src/main/java/com/example/sudokusolver/imagerec/Vector.java
@@ -0,0 +1,12 @@
+package com.example.sudokusolver.imagerec;
+
+class Vector {
+
+    double rho;
+    double theta;
+
+    Vector(double rho, double theta) {
+        this.rho = rho;
+        this.theta = theta;
+    }
+}
